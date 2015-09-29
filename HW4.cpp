2989a907b9044b690740fc73b6b68ffc058d32fc
JacobSiegel <jacobsiegel22@gmail.com>
2015-09-29 19:09:34 -0400

    now with comments

--- HW4.cpp
+++ HW4.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main(){ //tells the code where to start running
-  double ab = 1;
+  double ab = 1;//creates a variable ab that =1, needs to be double because it will end up being very large
   for (int i=0; i<=10; i++) { //for loop that counts every integer from i=0 to i=10
     for (int n=1; n<=i; n++) { //for loop that counts from 1 to i      
-      ab *= n;
+      ab *= n;//sets ab = to what ever ab was * n
     }
-    cout << i << " factorial is " << ab << endl;
-    ab = 1;
+    cout << i << " factorial is " << ab << endl; // displays ab
+    ab = 1; //resets ab back to one so that the new result doesnt get mulltiplied by the old factorial
   }
-  return 0;
+  return 0; //end code
 }
 
 
